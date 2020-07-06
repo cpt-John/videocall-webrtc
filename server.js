62f4bf1017c5a33c0d2ef4d19228ae6083077b37
John Francis <johnfrancis95815@gmail.com>
2020-07-06 17:11:53 +0530

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+let server;
+let port;
+
+const http = require("http");
+server = http.createServer(app);
+port = process.env.PORT || 3000;
+
+const io = require("socket.io")(server);
+const RoomService = require("./RoomService")(io);
+io.sockets.on("connection", RoomService.listen);
+io.sockets.on("error", (e) => console.log(e));
+app.use(express.static(__dirname + "/public"));
+app.get("*", function (req, res) {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+server.listen(port, () => console.log(`Server is running on port ${port}`));
