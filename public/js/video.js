5adb290b0e7f280acc031ad36e4d9a2d88173d4a
John Francis <johnfrancis95815@gmail.com>
2020-07-14 15:40:43 +0530

    new stun serv

--- public/js/video.js
+++ public/js/video.js
@@ -29,7 +29,7 @@ let gettingUserMedia = false;
 const config = {
   iceServers: [
     {
-      urls: ["stun:stun.l.google.com:19302"],
+      urls: ["stun:global.stun.twilio.com:3478?transport=udp"],
     },
   ],
 };
